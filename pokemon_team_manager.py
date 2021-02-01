e6fd95d30d45fa36ec3b73af834365f06256b91d
Stefanosdr <stefanosdragoutsis@gmail.com>
2021-02-01 21:12:22 +0000

    Added threads in team manager file

--- pokemon_team_manager.py
+++ pokemon_team_manager.py
@@ -1,11 +1,22 @@
 from pokemon_team import PokemonTriad
 from itertools import combinations
 from pokemon_class import Pokemon
+import concurrent.futures
+
 
 def get_pokemon_triads(pokemon_list):
-    pokemon_objects = [Pokemon(pokemon_name) for pokemon_name in pokemon_list]
-    pokemon_triads = [PokemonTriad(pokemon_triad) for pokemon_triad in list(combinations(pokemon_objects, 3))]
-    return pokemon_triads
+    def create_pokemon_object(pokemon_name):
+        try:
+            pokemon_object = Pokemon(pokemon_name)
+        except Exception as err:
+            print(f"Error while retrieving data for {pokemon_name}")
+        return pokemon_object
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(create_pokemon_object, pokemon_list)
+        print(results)
+    #pokemon_objects = [Pokemon(pokemon_name) for pokemon_name in pokemon_list]
+        pokemon_triads = [PokemonTriad(pokemon_triad) for pokemon_triad in list(combinations(results, 3))]
+        return pokemon_triads
 
 '''
 pokemon_names = ["Gardevoir", "Blastoise", "Golduck", "Venusaur", "Raichu", "Flygon"]
